e9f021d13118c666fa952b0b90a51bac44d787f5
不爱吃肉的小胖子 <2423862048@qq.com>
2022-05-01 22:07:09 +0800

    2022-05-01

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //配置端口
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    open: true
+  },
+  //不写的话会变成“严格模式”，关闭eslint.
+  lintOnSave: false,
 })
