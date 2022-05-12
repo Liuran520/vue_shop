10e5e5306a11d4ecd2c0dd544c54a6055b00ef97
不爱吃肉的小胖子 <2423862048@qq.com>
2022-05-12 19:52:44 +0800

    2022-05-12

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import { Button ,Form, FormItem, Input,Message,Container,Header,Aside,Main,Menu,
     Submenu,MenuItem,Breadcrumb,BreadcrumbItem,Card,Row,Col,Table,TableColumn,Switch,Tooltip,
-    Pagination,Dialog,MessageBox,Tag
+    Pagination,Dialog,MessageBox,Tag,Select,Option,Tree,Cascader
 } from 'element-ui'
 
 Vue.use(Button).use(Form).use(FormItem).use(Input)
 .use(Container).use(Header).use(Aside)
 .use(Main).use(Menu).use(MenuItem).use(Submenu)
 .use(Breadcrumb).use(BreadcrumbItem).use(Card).use(Row).use(Col).use(Table).use(TableColumn)
-.use(Switch).use(Tooltip).use(Pagination).use(Dialog).use(Tag)
+.use(Switch).use(Tooltip).use(Pagination).use(Dialog).use(Tag).use(Select).use(Option).use(Tree)
+.use(Cascader)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import User from '../components/User/User.vue'
 import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
+import Cate from '../components/goods/Cate.vue'
 
 Vue.use(VueRouter)
 
@@ -18,7 +20,9 @@ const routes = [
      children:[
        {path:'/welcome',component:Welcome},
        {path:'/users',component:User},
-       {path:'/rights',component:Rights}
+       {path:'/rights',component:Rights},
+       {path:'/roles',component:Roles},
+       {path:'/categories',component:Cate}
      ]
   }
 ]
