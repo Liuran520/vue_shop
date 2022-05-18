bef364b73d3a0da1dbe447815d8356040977430c
不爱吃肉的小胖子 <2423862048@qq.com>
2022-05-18 20:13:04 +0800

    2022-05-18

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import { Button ,Form, FormItem, Input,Message,Container,Header,Aside,Main,Menu,
     Submenu,MenuItem,Breadcrumb,BreadcrumbItem,Card,Row,Col,Table,TableColumn,Switch,Tooltip,
-    Pagination,Dialog,MessageBox,Tag,Select,Option,Tree,Cascader,Alert,TabPane,Tabs
+    Pagination,Dialog,MessageBox,Tag,Select,Option,Tree,Cascader,Alert,TabPane,Tabs,Steps,Step,CheckboxGroup,
+    Checkbox,Upload
 } from 'element-ui'
 
 Vue.use(Button).use(Form).use(FormItem).use(Input)
@@ -9,6 +10,7 @@ Vue.use(Button).use(Form).use(FormItem).use(Input)
 .use(Main).use(Menu).use(MenuItem).use(Submenu)
 .use(Breadcrumb).use(BreadcrumbItem).use(Card).use(Row).use(Col).use(Table).use(TableColumn)
 .use(Switch).use(Tooltip).use(Pagination).use(Dialog).use(Tag).use(Select).use(Option).use(Tree)
-.use(Cascader).use(Alert).use(TabPane).use(Tabs)
+.use(Cascader).use(Alert).use(TabPane).use(Tabs).use(Steps).use(Step).use(CheckboxGroup).use(Checkbox)
+.use(Upload)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
